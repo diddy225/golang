d7204dc81f1826c404f79e5f02bec947dd58a4d7
Matthew C <matthew.j.carpenter1@gmail.com>
2019-03-20 18:40:39 -0400

    added server

--- hello-world.go
+++ hello-world.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	fmt.Println("Hello world")
+	router := mux.NewRouter()
+	router.HandleFunc("/", handler).Methods("GET")
+
+	http.ListenAndServe(":8080", router)
+}
+
+func handler(respond http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(respond, "Hello World!")
 }
